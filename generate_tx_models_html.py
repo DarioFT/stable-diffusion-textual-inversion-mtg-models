b363089eac0f064733806bf8335b8483d7101e98
darioft <dariofernandeztorre@gmail.com>
2022-10-17 20:52:48 -0300

    Update generate_tx_models_html.py

--- generate_tx_models_html.py
+++ generate_tx_models_html.py
@@ -171,13 +171,39 @@ for model_name in models_list:
 </div>
             """
         html_struct = html_struct + '</div></div>'
+
+    html_struct = html_struct + """
+
+</div>
+
+  <div class="tab-pane fade" id="{model_name}-model-tab" role="tabpanel" aria-labelledby="{model_name}-model-tab">
+
+<div class="row">
+
+"""
+
+# Most repos have 3 concept images but some have more or less
+# We gotta make sure only 3 are shown
+
+    img_count = 3
+    if len(concept_images) < 3:
+        img_count = len(concept_images)
+
+    for x in range(img_count):
+        html_struct = html_struct + f"""
+
+    <div class="col-sm">
+      <!-- <img class="thumbnail mx-auto lazy-load img-fluid" data-src="https://huggingface.co/sd-concepts-library/{model_name}/resolve/main/{concept_images[x]}">-->
+      <img class="thumbnail mx-auto img-fluid" loading="lazy" src="https://huggingface.co/sd-concepts-library/{model_name}/resolve/main/{concept_images[x]}">
+    </div>
+                """
+        html_struct = html_struct + '</div></div>'
+        
     i = i + 1
 
 html_struct = html_struct + """
 
 </div>
-
-
 
   </div>
   <script>
